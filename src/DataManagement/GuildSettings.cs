432ed4af01e22b385f7678aed714a7e33204167c
SlidyDev <michal2006.2006@gmail.com>
2022-12-09 16:56:28 +0100

    Cleanup

--- src/DataManagement/GuildSettings.cs
+++
@@ -1,9 +0,0 @@
-using Newtonsoft.Json;
-
-namespace ScarchivesBot.DataManagement;
-
-internal class GuildSettings : Settings
-{
-    [JsonProperty("roles")]
-    public List<long> Roles { get; set; } = new();
-}
